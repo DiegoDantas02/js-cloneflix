48226e6eaefa877b54ff1c138b881c90033b799a
Diego R Dantas <112526586+DiegoDantas02@users.noreply.github.com>
2022-10-25 22:28:33 -0300

    azak flix

---
+++ app.js
@@ -0,0 +1,15 @@
+let filme1 = 'imagens/filme01.jpg'
+let filme2 = 'imagens/filme02.jpg'
+let filme3 = 'imagens/filme03.jpg'
+let filme4 = 'imagens/crime.jpg'
+let filme5 = 'imagens/oculto.jfif'
+let filme6 = 'imagens/soldado.jfif'
+let filme7 = 'imagens/supernatural.jpg'
+
+document.querySelector(".container").innerHTML += "<img src='" + filme1 + " ' > "
+document.querySelector(".container").innerHTML += "<img src='" + filme2 + " ' > "
+document.querySelector(".container").innerHTML += "<img src='" + filme3 + " ' > "
+document.querySelector(".container").innerHTML += "<img src='" + filme4 + " ' > "
+document.querySelector(".container").innerHTML += "<img src='" + filme5 + " ' > "
+document.querySelector(".container").innerHTML += "<img src='" + filme6 + " ' > "
+document.querySelector(".container").innerHTML += "<img src='" + filme7 + " ' > "
